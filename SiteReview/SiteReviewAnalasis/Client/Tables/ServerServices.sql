db3754475019eb05dbe1f96dcb71f8a6c18127ff
Sharon Rimer <crs2007@gmail.com>
2019-06-11 08:58:03 +0300

    Update ServerServices.sql

--- SiteReview/SiteReviewAnalasis/Client/Tables/ServerServices.sql
+++ SiteReview/SiteReviewAnalasis/Client/Tables/ServerServices.sql
@@ -5,7 +5,8 @@
     [StartupType]     INT              NOT NULL,
     [Status]          INT              NOT NULL,
     [StatusDesc]      NVARCHAR (255)   NOT NULL,
-    [ServiceAccount]  NVARCHAR (255)   NOT NULL
+    [ServiceAccount]  NVARCHAR (255)   NOT NULL,
+    [instant_file_initialization_enabled] BIT              NULL
 );
 
 
